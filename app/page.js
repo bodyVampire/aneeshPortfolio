ac1ad8f53a5dbabe3c426f3e1350f2d38b59e983
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-01-24 14:39:37 +0530

    add config

--- app/page.js
+++ app/page.js
@@ -5,12 +5,18 @@ import 'aos/dist/aos.css';
 import { MdDarkMode } from "react-icons/md";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaLinkedin,FaYoutube } from "react-icons/fa";
-import image from "next/image";
+import "./globals.css"
+
+
 export default function Home() {
   useEffect(() => {
     AOS.init({
-         duration: 800,
-         once: false,
+      
+      easing: 'ease-in-out',
+      offset: 50,
+      delay: 0,
+      once: true,
+      mirror: false,
        })
  }, [])
   
@@ -19,7 +25,7 @@ export default function Home() {
 <main className=" bg-white px-5">
   <section className="min-h-screen">
   <nav className="py-10 mb-12 flex justify-between">
-<h1 className="text-xl font-bold">Developer</h1>
+<h1 className="text-xl font-bold" data-aos="fade-right" data-aos-duration="2000">--Developer--</h1>
 <ul className="flex items-center">
   <li>
   <MdDarkMode className="cursor-pointer text-2xl" />
@@ -29,20 +35,24 @@ export default function Home() {
 </ul>
 </nav>
 <div className="text-center p-10">
-<h2 className="text-3xl font-bold text-blue-500 py-2">Aneesh Maury</h2>
-<h3 className="text-2xl py-2">Developer And Designer</h3>
+<h2 className="text-6xl font-bold text-black-500 py-2" data-aos="zoom-in"  data-aos-duration="2000" >Aneesh</h2>
+<h3 className="text-2xl py-2">Front-End <span className="font-bold" data-aos="fade-left" data-aos-duration="2000">Dev</span> </h3>
 <p className="text-md py-5 leading-8 text-gray-800">
 With a strong foundation in computer science, I am currently honing my skills and knowledge through an internship at INFORTRIXS,
  specializing in Front-End Development.
 </p>
 </div>
-<div className="text-4xl flex justify-center gap-10" data-aos="fade-up" >
-<FaSquareXTwitter />
-<FaLinkedin />
-<FaYoutube />
+<div className="text-4xl flex justify-center gap-10" >
+<a href="https://twitter.com/codewithaneesh" target="_blank"data-aos="fade-up" data-aos-duration="1500"><FaSquareXTwitter/></a>
+<a href="https://www.linkedin.com/in/aneeshmaury/" target="_blank" data-aos="fade-up" data-aos-duration="1800"><FaLinkedin /></a>
+<a href="https://www.youtube.com/@codewithaneesh" target="blank" data-aos="fade-up" data-aos-duration="2100"><FaYoutube /></a>
+
 </div>
-<img src="/images/imagelogo.jpg" alt="logo" className="flex justify-center rounded-full w-80 h-80 mt-10 mx-auto"/>
+<img src="/images/imagelogo.jpg" alt="logo" className="flex justify-center rounded-full w-80 h-80 mt-10 mx-auto"   data-aos="fade-up"  data-aos-duration="2000" />
   </section>
+
+
+
 </main>
    
   )
